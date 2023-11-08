ef8480a957e08616f7754ec3bc56c8d504971b4c
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-11-08 12:54:47 +0900

    feat: 로또 숫자 입력을 반점 기준으로 분리

--- src/main/java/lotto/utils/StringUtil.java
+++ src/main/java/lotto/utils/StringUtil.java
@@ -1,7 +1,15 @@
 package lotto.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringUtil {
     public static int divideByOneThousand(String number) {
         return Integer.parseInt(number) / 1000;
+    }
+
+
+    public static List<String> splitByCommas(String string) {
+        return Arrays.stream(string.split(",")).toList();
     }
 }
--- src/test/java/lotto/utils/StringUtilTest.java
+++ src/test/java/lotto/utils/StringUtilTest.java
@@ -2,11 +2,17 @@ package lotto.utils;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class StringUtilTest {
     @Test
     void divideByOneThousand() {
         assertThat(StringUtil.divideByOneThousand("2000")).isEqualTo(2);
+    }
+
+    @Test
+    void splitByCommas() {
+        assertThat(StringUtil.splitByCommas("1,2,3,4,5,6")).isEqualTo(List.of("1", "2", "3", "4", "5", "6"));
     }
 }
