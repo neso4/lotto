28e4d940f542d8ed31478bd7d95659c7113081f3
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-11-08 19:04:51 +0900

    test(RanksTest): 테스트 내 오류 수정

--- src/test/java/lotto/domain/RanksTest.java
+++ src/test/java/lotto/domain/RanksTest.java
@@ -11,9 +11,9 @@ class RanksTest {
     @DisplayName("등수를 우선순위 정렬해서 저장한다.")
     @Test
     void 우선순위_정렬후_저장() {
-        Ranks ranks = new Ranks(List.of(Rank.LAST, Rank.FIRST));
+        Ranks ranks = new Ranks(List.of(Rank.SECOND, Rank.FIRST));
 
-        assertThat(ranks.getRanks()).isEqualTo(List.of(Rank.FIRST, Rank.LAST));
+        assertThat(ranks.getRanks()).isEqualTo(List.of(Rank.FIRST, Rank.SECOND));
     }
 
     @DisplayName("마지막_등수는_제외하고_반환")
