883b6f834f3c568c1c9cb481fb6cb4d639c66995
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-11-08 21:29:15 +0900

    test(StringUtilTest): 로또 출력 형식 기능 관련 유틸리티 기능 테스트 추가

--- src/test/java/lotto/utils/StringUtilTest.java
+++ src/test/java/lotto/utils/StringUtilTest.java
@@ -15,4 +15,14 @@ class StringUtilTest {
     void splitByCommas() {
         assertThat(StringUtil.splitByCommas("1,2,3,4,5,6")).isEqualTo(List.of("1", "2", "3", "4", "5", "6"));
     }
+
+    @Test
+    void divideNumsByCommas() {
+        assertThat(StringUtil.divideNumsByCommas(List.of(1, 2, 3, 4, 5, 6))).isEqualTo("1, 2, 3, 4, 5, 6");
+    }
+
+    @Test
+    void coverWithBrackets() {
+        assertThat(StringUtil.coverWithBrackets("1, 2, 3, 4, 5, 6")).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
 }
