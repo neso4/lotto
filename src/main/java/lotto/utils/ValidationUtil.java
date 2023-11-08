7ed43e491d4b375162eebbc67b54e29e8ebe1178
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-11-08 12:31:59 +0900

    style(ValidationUtil): 조건문 스타일 변경

--- src/main/java/lotto/utils/ValidationUtil.java
+++ src/main/java/lotto/utils/ValidationUtil.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public class ValidationUtil {
     public static void validateIsMoneyThousandUnit(String number) {
-        if (Integer.parseInt(number) % 1000 != 0) throw new IllegalArgumentException("로또 구입 금액은 1000원 단위여야 합니다.");
+        if (Integer.parseInt(number) % 1000 != 0) {
+            throw new IllegalArgumentException("로또 구입 금액은 1000원 단위여야 합니다.");
+        }
     }
 
     public static void validateIsMoneyDigit(String input) {
@@ -16,10 +18,14 @@ public class ValidationUtil {
     }
 
     public static void validateNoDuplicatedNumbers(List<Integer> numbers) {
-        if (numbers.size() != numbers.stream().distinct().count()) throw new IllegalArgumentException("로또 번호는 서로 중복되지 않은 숫자여야 합니다.");
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("로또 번호는 서로 중복되지 않은 숫자여야 합니다.");
+        }
     }
 
     public static void validateHasSixNumbers(List<Integer> numbers) {
-        if (numbers.size() != 6) throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
+        }
     }
 }
