674223bdc898916ba2118d28c1b2a70a63640ecc
kimdohee58 <kimdohee58@gmail.com>
2024-06-17 04:17:21 +0900

    240617

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
         // TODO: 프로그램 구현
+        Money money = new Money();
+        money.showMain();
     }
 }
